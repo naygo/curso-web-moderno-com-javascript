523bcbdf2ca563da385a0c1ef0ca36e2ee5e5574
Nayla Gomes <nayla.cgs@gmail.com>
2020-07-11 15:07:08 -0300

    Tratamento de Erro (Try/Catch/Throw )

---
+++ Fundamentos/erro.js
@@ -0,0 +1,25 @@
+function tratarErroLancar (erro) {
+    //throw new Error('Ocorreu um erro...')
+    //throw 10
+    //throw true
+    //throw 'Mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase + '!!!')
+
+    } catch (e) {
+        tratarErroLancar(e)
+    } finally {
+        console.log('Final')
+    }
+}
+
+const obj = { nome: 'Nayla' } // erro no 'nome'
+imprimirNomeGritado(obj)
