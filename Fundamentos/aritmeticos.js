e10d6b3f358b21e62fa9ba230876dbd097938936
Nayla Gomes <nayla.cgs@gmail.com>
2020-07-11 15:05:35 -0300

    Operadores: Aritméticos

---
+++ Fundamentos/aritmeticos.js
@@ -0,0 +1,12 @@
+const [ a, b, c, d ] = [ 3, 5, 1, 15 ]
+
+const soma = a + b + c + d
+const subtracao = b - d
+const multiplicacao =  a * b
+const exponenciacao = a ** b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, exponenciacao, divisao, modulo)
+console.log(-divisao)
+
