4c0a646a056dc0eae4c8d5dcabbcc85e79eb0d86
Nayla Gomes <nayla.cgs@gmail.com>
2020-07-19 13:54:17 -0300

    Sistema de módulos: importando e exportando

---
+++ Node/moduloA.js
@@ -0,0 +1,6 @@
+// exemplos de exportar
+// podem ser acessados em outro arquivo
+
+this.ola = 'Fala pessoal' 
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até o próximo exemplo'
---
+++ Node/moduloB.js
@@ -0,0 +1,8 @@
+// forma mais clássica atribuindo objeto
+
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite() {
+        return 'Boa noite'
+    }
+}
